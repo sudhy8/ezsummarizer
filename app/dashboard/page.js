0559a6d77cccf1507bc65e2ccf9efb7b026a688d
Sudhy Sukumaran <sudhy8@gmail.com>
2024-07-20 14:36:13 -0400

    upload

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -13,7 +13,7 @@ import { Grid } from '@mui/material';
 import styles from './page.module.css';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
-import { useForm } from 'react-hook-form';
+import { set, useForm } from 'react-hook-form';
 import Typewriter from '../TypingEffect';
 import CircularProgress from '@mui/material/CircularProgress';
 import Stack from '@mui/material/Stack';
@@ -45,6 +45,8 @@ import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded';
 import Menu from '@mui/material/Menu';
 import FileUploadRoundedIcon from '@mui/icons-material/FileUploadRounded';
 import MenuItem from '@mui/material/MenuItem';
+import { Dropbox } from 'dropbox';
+
 const folderIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fill="#0069ff" width="64px" height="64px"><path d="M 15.375 15 C 12.852469 15 10.787109 17.066423 10.787109 19.587891 L 10.787109 46.251953 C 10.787109 48.769711 11.338238 50.749451 12.6875 52.072266 C 14.036762 53.39508 16.013124 53.902344 18.470703 53.902344 L 48.324219 53.902344 C 50.188281 53.902344 51.707272 53.59715 52.890625 52.916016 C 54.073978 52.234881 54.870264 51.132805 55.166016 49.845703 C 56.442844 44.292168 56.280824 38.57964 56.257812 31.380859 C 56.257812 28.644179 54.07666 26.496842 51.460938 26.015625 A 1.0001 1.0001 0 0 0 51.279297 26 L 49.757812 26 C 49.676153 23.24049 47.421878 21 44.644531 21 L 33.257812 21 C 32.265487 21 31.364119 20.436916 30.929688 19.544922 L 29.974609 17.580078 C 29.208038 16.003496 27.603284 15 25.849609 15 L 15.375 15 z M 15.375 17 L 25.849609 17 C 26.841934 17 27.742353 17.563661 28.175781 18.455078 L 29.130859 20.419922 C 29.898428 21.995927 31.504139 23 33.257812 23 L 44.644531 23 C 46.335475 23 47.64211 24.331301 47.728516 26 L 26.554688 26 A 1.0001 1.0001 0 0 0 26.375 26.015625 C 23.758353 26.496793 21.578125 28.644483 21.578125 31.380859 C 21.577887 31.395359 21.533535 34.137681 21.361328 37.291016 C 21.188666 40.452676 20.834997 44.112156 20.416016 45.427734 A 1.0001 1.0001 0 0 0 20.416016 45.429688 C 19.818162 47.308163 18.905745 47.638151 18.244141 47.511719 C 17.582536 47.385286 16.787109 46.630599 16.787109 44.953125 L 16.787109 41 A 1.0001 1.0001 0 1 0 14.787109 41 L 14.787109 44.953125 C 14.787109 47.321651 16.086995 49.135995 17.869141 49.476562 C 19.651286 49.81713 21.531166 48.514681 22.320312 46.035156 C 22.930332 44.119735 23.182334 40.604527 23.357422 37.398438 C 23.53251 34.192347 23.578125 31.396484 23.578125 31.396484 A 1.0001 1.0001 0 0 0 23.578125 31.380859 C 23.578125 29.736031 24.94645 28.338764 26.707031 28 L 47.787109 28 L 47.787109 35.447266 A 1.0001 1.0001 0 1 0 49.787109 35.447266 L 49.787109 28 L 51.128906 28 C 52.888436 28.338728 54.257812 29.73634 54.257812 31.380859 A 1.0001 1.0001 0 0 0 54.257812 31.384766 C 54.280853 38.609967 54.417715 44.175074 53.216797 49.398438 C 53.029549 50.213335 52.645475 50.750228 51.892578 51.183594 C 51.139681 51.616959 49.964156 51.902344 48.324219 51.902344 L 18.470703 51.902344 C 16.286282 51.902344 14.919629 51.458014 14.087891 50.642578 C 13.256152 49.827143 12.787109 48.483195 12.787109 46.251953 L 12.787109 19.587891 C 12.787109 18.147358 13.933531 17 15.375 17 z" /></svg>
 const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
@@ -60,7 +62,41 @@ export default function Home() {
     const { register, reset, handleSubmit, formState: { errors } } = useForm();
     const [session, setSession] = useState(null)
     const [open, setOpen] = React.useState(false);
+    const [fileOpen, setFileOpen] = React.useState(false);
+
     const [audioOpen, setAudioOpen] = React.useState(false);
+    const notify = () => toast.success("Recording Started...!");
+    const notify2 = () => toast.error("Recording Stopped!");
+
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+
+    const handleUpload = () => {
+        const accessToken = 'sl.B5YkJjo840ZDd6wlfzytJqRBCQDMiKXReijTCu9iBKwh63eZu9YHvColROrBsrdph_e-gTWB-uLpif-M5xI7HzLNNyiHN4T2-aBypq32ZiNBlW1dUjYwBX8hWQgQxCFmWWMGcXvjJa2ghBqqIjAGm9k'; // Replace with your access token
+        const dbx = new Dropbox({ accessToken });
+
+        const reader = new FileReader();
+        reader.onload = function (event) {
+            const fileContent = event.target.result;
+
+            dbx.filesUpload({
+                path: '/' + file.name,
+                contents: fileContent
+            })
+                .then((response) => {
+                    console.log('File uploaded successfully:', response);
+                })
+                .catch((error) => {
+                    console.error('Error uploading file:', error);
+                });
+        };
+
+        reader.readAsArrayBuffer(file);
+    };
+
 
     const [files, setFiles] = React.useState({});
     const [anchorEl, setAnchorEl] = React.useState(null);
@@ -251,8 +287,69 @@ export default function Home() {
         console.log("folder", value);
         event.stopPropagation();
     }
+
+    const handleFileClose = () => { 
+        setFileOpen(false);
+    }
+    
     return (
         <div >
+
+            
+
+
+            <ToastContainer hideProgressBar={true} closeOnClick theme={"dark"} />
+
+            <Dialog
+                open={fileOpen}
+                onClose={handleFileClose}
+
+            >
+
+                <DialogContent style={{ padding: '0px' }}>
+
+
+                    <Grid container style={{ minWidth: '600px', width: '40%' }}>
+
+                        <Grid item xs={12} style={{ padding: '18px 20px 10px' }}>
+                            <Typography variant="p" component="div" style={{ fontFamily: 'var(--font-poppins-bold)', fontWeight: 500, fontSize: '16px' }}>Upload your Video/Audio 🎥 </Typography>
+                            
+
+                        </Grid>
+                    </Grid>
+                    <Divider style={{ margin: '10px 0' }} />
+
+                    <Grid container style={{ padding: '15px 20px' }}>
+
+                        <Grid item xs={12} >
+                            <div className="output" style={{ maxHeight: '250px', overflow: 'auto' }}>
+                                <input type="file" onChange={handleFileChange} />
+                               
+                            </div>
+
+                        </Grid>
+
+
+                        <Grid item xs={12} style={{ padding: '15px 0px 0px', display: 'flex', justifyContent: 'flex-end' }}>
+
+                           
+                            <Button onClick={() => { setFileOpen(false) }}style={{ color: "#0069ff", boxShadow: "none", fontFamily: "var(--font-poppins-bold)", textTransform: "none", fontWeight: 500 }}>Cancel</Button>
+                            <Button onClick={handleUpload } disabled={isListening} variant="contained" style={{ background: "#cce4ff", color: "#0069ff", boxShadow: "none", fontFamily: "var(--font-poppins-bold)", textTransform: "none", fontWeight: 500 }}>Upload</Button>
+
+
+                        </Grid>
+                    </Grid>
+
+
+
+
+                </DialogContent>
+
+
+            </Dialog>
+
+
+
 
             <Dialog
                 open={audioOpen}
@@ -302,9 +399,9 @@ export default function Home() {
 
                         <Grid item xs={12} style={{ padding: '15px 0px 0px', display: 'flex', justifyContent: 'flex-end' }}>
 
-                            <Button onClick={() => { stopListening();setAudioOpen(false) }} disabled={!isListening} style={{ color: "red", boxShadow: "none", fontFamily: "var(--font-poppins-bold)", textTransform: "none", fontWeight: 500 }}>Stop & Close</Button>
-                            <Button onClick={stopListening} disabled={!isListening} style={{ color: "#0069ff", boxShadow: "none", fontFamily: "var(--font-poppins-bold)", textTransform: "none", fontWeight: 500 }}>Stop & Submit</Button>
-                            <Button onClick={startListening} disabled={isListening} variant="contained" style={{ background: "#cce4ff", color: "#0069ff", boxShadow: "none", fontFamily: "var(--font-poppins-bold)", textTransform: "none", fontWeight: 500 }}>Start Recording</Button>
+                            <Button onClick={() => { stopListening(); setAudioOpen(false); notify2() }} disabled={!isListening} style={{ color: "red", boxShadow: "none", fontFamily: "var(--font-poppins-bold)", textTransform: "none", fontWeight: 500 }}>Stop & Close</Button>
+                            <Button onClick={() => { stopListening(); notify2() }} disabled={!isListening} style={{ color: "#0069ff", boxShadow: "none", fontFamily: "var(--font-poppins-bold)", textTransform: "none", fontWeight: 500 }}>Stop & Submit</Button>
+                            <Button onClick={() => { startListening(); notify()}} disabled={isListening} variant="contained" style={{ background: "#cce4ff", color: "#0069ff", boxShadow: "none", fontFamily: "var(--font-poppins-bold)", textTransform: "none", fontWeight: 500 }}>Start Recording</Button>
 
 
                         </Grid>
@@ -500,7 +597,7 @@ export default function Home() {
                             display: 'flex',
                             justifyContent: 'center'
                         }}>
-                            <Button variant="contained" style={{ width: '100%', background: "#cce4ff", color: "#0069ff", boxShadow: "none", fontFamily: "var(--font-poppins-bold)", textTransform: "none", fontWeight: 500 }} startIcon={<FileUploadRoundedIcon />}>
+                            <Button onClick={()=>setFileOpen(true)} variant="contained" style={{ width: '100%', background: "#cce4ff", color: "#0069ff", boxShadow: "none", fontFamily: "var(--font-poppins-bold)", textTransform: "none", fontWeight: 500 }} startIcon={<FileUploadRoundedIcon />}>
                                 Upload
                                 
                             </Button>
