ae1af1fab872573e5f7079812f76c20753f7a110
Sudhy Sukumaran <sudhy8@gmail.com>
2024-07-21 15:36:55 -0400

    Check

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-// module.exports = {
-//     env: {
-//         NEXT_PUBLIC_SUPABASE_URL:'https://qcacpcxtqeuoeyzsbfro.supabase.co',
-//         NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjYWNwY3h0cWV1b2V5enNiZnJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0OTkzNjIsImV4cCI6MjAzNDA3NTM2Mn0.4fp0FdIaWj0uyYjeqvCVwuTF4JVzVvK_ivI1YFtTI3c',
-//         DROPBOX_CLIENT_ID: 'nmotgy9ip4h9evc',
-//         DROPBOX_CLIENT_SECRET: '8ukac8t0fzyh68z',
-//         DROPBOX_REFRESH_TOKEN: '8veR7yVbibYAAAAAAAAAXUnuGHpdBh3chgdHX8cwRfU'
-//     },
-// }
-
-// module.exports = nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
