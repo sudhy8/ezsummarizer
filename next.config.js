fc74b2d079eed456a6ecebb532f08e99003abe7d
Sudhy Sukumaran <sudhy8@gmail.com>
2024-07-21 15:17:05 -0400

    Youtube URL added

--- next.config.js
+++ next.config.js
@@ -15,12 +15,10 @@
 
 const nextConfig = {
     env: {
-                NEXT_PUBLIC_SUPABASE_URL:'https://qcacpcxtqeuoeyzsbfro.supabase.co',
-                NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjYWNwY3h0cWV1b2V5enNiZnJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0OTkzNjIsImV4cCI6MjAzNDA3NTM2Mn0.4fp0FdIaWj0uyYjeqvCVwuTF4JVzVvK_ivI1YFtTI3c',
-               
+        
     },
     reactStrictMode: false,
-   
+
 };
 
 module.exports = nextConfig;
