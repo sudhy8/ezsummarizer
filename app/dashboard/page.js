3d5e73eab183957a1f69867791ac1d28693e3865
Sudhy Sukumaran <sudhy8@gmail.com>
2024-07-21 15:36:14 -0400

    Check

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -124,7 +124,7 @@ export default function Home() {
 
 
                         const client = new AssemblyAI({
-                            apiKey: "2bf1061ce19a48369e3adbabffdb0a44"
+                           
                         })
 
                         const audioUrl = modifyDropboxUrl(sharedLinkResponse?.result?.url);
