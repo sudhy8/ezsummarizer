8e002116de951231556267d86f05588e77011e99
sudhy44 <66510639+sudhy44@users.noreply.github.com>
2024-08-10 07:25:03 -0400

    Dropbox api updated

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         DROPBOX_CLIENT_ID: 'nmotgy9ip4h9evc',
         DROPBOX_CLIENT_SECRET: '8ukac8t0fzyh68z',
         DROPBOX_REFRESH_TOKEN: '8veR7yVbibYAAAAAAAAAX_4AK0GSlrdktwvfLwDgRv0',
-        DROPBOX_ACCESS_TOKEN: 'sl.B6tfndSQpMLdk-51OF0Im-ztr_wOjcFe4XMg0YyGHVw8ZXk5dye6ORTOkdHNswPjapsnrUxgqbfOS1dJDD0unaOuMLOf_UZ8e4f8GfsCkpGm6RO_CCNS_4s_dwIU0VBHW1mhMBMKfI5u1snPHNx_6o0'
+        DROPBOX_ACCESS_TOKEN: 'sl.B6s4e74zcaYIBOSUgntbV69Kgd4SysklKkIFSV_v3xPvJFPqR0AvnFYYXj8xB7jfGzFwxP1aTwyA7dq03EArf5ThQVRo1eSZ65gNJwayIv9FC7c7BKWdA0VYediKbpecWIDkeThtx5pn4of8S_Jb7fU'
     },
     reactStrictMode: false,
 
