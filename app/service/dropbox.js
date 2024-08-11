46de8b08d3197c376049be9a3d5551058d24e112
sudhy sukumaran <48507263+sudhy8@users.noreply.github.com>
2024-08-11 09:22:44 -0400

    Keys removed

--- app/service/dropbox.js
+++ app/service/dropbox.js
@@ -1,13 +1,10 @@
 var Dropbox = require("dropbox").Dropbox;
 const dbx = new Dropbox({
-    // clientId: process.env.DROPBOX_CLIENT_ID,
-    // clientSecret: process.env.DROPBOX_CLIENT_SECRET,
-    // refreshToken: process.env.DROPBOX_REFRESH_TOKEN,
-    // token_access_type: 'offline',
+    clientId: process.env.DROPBOX_CLIENT_ID,
+    clientSecret: process.env.DROPBOX_CLIENT_SECRET,
+    refreshToken: process.env.DROPBOX_REFRESH_TOKEN,
+    token_access_type: 'offline',
 
-    clientId: 'ons7bx7cm7zkdzz',
-    clientSecret: 'qajyb859d3gcl7u',
-    refreshToken: 'ifogVAS3OjEAAAAAAAAAAbDWRAWX0kiwszEjK9OzlQIg5LVDanjB5Jqx7Ig13e9W',
 });
 
 export default dbx;
